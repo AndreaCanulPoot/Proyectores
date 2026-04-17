40d205bb925a49fbcf740a06396ecce31031e7e3
Andrea Sac-nicte Canul Poot <23470137@campeche.tecnm.mx>
2026-04-17 08:20:44 -0600

    Esta es la primera versión de Proyectores

---
+++ WebApp/Controllers/ProyectoresController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class ProyectoresController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            string html = "<h2>Inicio </h2>";
+            html += $"<p>Aqu&iacute se mostrar&aacute la lista de proyectores registrados</p>";
+            return Content(html, "text/html");
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Content
+                (
+                "<h2>Nuevo Proyector </h2>"
+                + $"<p>Aqu&iacute se mostrar&aacute la informaci&oacuten de un proyector {id}<p>",
+                "text/html"
+                );
+        }
+
+
+        public IActionResult Delete(int id)
+        {
+            return Content
+                (
+                "<h2>Dar de baja</h2>"
+                + $"<p>Se dar&aacute de baja el proyector {id}</p>",
+                "text/html"
+                );
+        }
+    }
+}
